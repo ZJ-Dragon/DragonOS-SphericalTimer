6a1fbca0e67cca0871d67c6feba2d672b096c53c
Fighter <fighter23317dragon@gmail.com>
2023-06-29 08:38:53 +0000

    A simple update
    Signed-off-by: Fighter <fighter23317dragon@gmail.com>

--- DragonOS-SphericalTimer.ino
+++ DragonOS-SphericalTimer.ino
@@ -1,5 +1,5 @@
 // DragonOS_SphericalTimer_v.17.0.23621.1001
-// This is an unstable version to test.
+// This is an unstable version to test
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
@@ -14,7 +14,7 @@ U8G2_FOR_ADAFRUIT_GFX u8g2_for_adafruit_gfx;
 
 /* Create an array to represent the functions that will be used */
 
-String Mode[]={((String)("STOP WATCH")), ((String)("COUNT DOWN")), ((String)("MIX")), ((String)("TODO")), ((String)("COUNTDOWN DAY"))};
+String Mode[]={((String)("STOP WATCH")), ((String)("COUNT DOWN")), ((String)("COMPREHENSIVE")), ((String)("TODO")), ((String)("COUNTDOWN DAY"))};
 
 void setup() {
   // put your setup code here, to run once:
@@ -26,7 +26,7 @@ void setup() {
   u8g2_for_adafruit_gfx.begin(tft);
   tft.fillScreen((0));
 
-  /* Initialize Font */
+  /* Initialize Fonts */
 
   u8g2_for_adafruit_gfx.setFont(u8g2_font_timB18_tf);
   u8g2_for_adafruit_gfx.setFont(u8g2_font_wqy12_t_gb2312);
@@ -36,18 +36,19 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   /* Power on and boot the device -------------------- */
-  
-  u8g2_for_adafruit_gfx.setCursor(0,20);
-  u8g2_for_adafruit_gfx.setForegroundColor((65126));
+  /* Exactly, there is a vitural boot code ----------- */
+
+  u8g2_for_adafruit_gfx.setCursor(0,20);                      // Locate where to display
+  u8g2_for_adafruit_gfx.setForegroundColor((65126));          // Clear the display
   u8g2_for_adafruit_gfx.print("DragonTimer");                 // Display the boot logo
 
   delay(3000);                                                // Special effect for wait
 
-  u8g2_for_adafruit_gfx.setCursor(0,20);
-  u8g2_for_adafruit_gfx.setForegroundColor((65126));
+  u8g2_for_adafruit_gfx.setCursor(0,20);                      
+  u8g2_for_adafruit_gfx.setForegroundColor((65126));          
   u8g2_for_adafruit_gfx.print("DragonOS SphericalTimer");     // Display the OS name
 
-  u8g2_for_adafruit_gfx.setCursor(0,40);
-  u8g2_for_adafruit_gfx.setForegroundColor((65126));
+  u8g2_for_adafruit_gfx.setCursor(0,40);                      
+  u8g2_for_adafruit_gfx.setForegroundColor((65126));          
   u8g2_for_adafruit_gfx.print("v.17.0.23621.1000");           // Display the version info
 }
